3ea152a23c7e572938e6d9d3f75383357c667de5
mariogate <161507991+mariogate@users.noreply.github.com>
2025-06-12 13:58:42 +0300

    Delete models directory

--- models/db.js
+++
@@ -1,8 +0,0 @@
-// Placeholder for database setup. In a real application this would
-// configure and export a connection to PostgreSQL.
-
-module.exports = {
-  query: async () => {
-    throw new Error('Database not implemented');
-  },
-};
